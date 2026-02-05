4eb944f4accd39a513abbd5fa1c83e29a090b6d8
jiwonchoi <jiwonchoi@jiwonchoiui-MacBookAir.local>
2026-02-05 14:01:47 +0900

    fix: version added

--- src/main/java/com/teamgold/goldenharvest/domain/inventory/command/domain/lot/Lot.java
+++ src/main/java/com/teamgold/goldenharvest/domain/inventory/command/domain/lot/Lot.java
@@ -36,6 +36,9 @@ public class Lot {
     @Column(name = "lot_status", nullable = false)
     private LotStatus lotStatus;
 
+    @Version
+    private Long version;
+
 	public enum LotStatus {
 		AVAILABLE,
 		ALLOCATED,
